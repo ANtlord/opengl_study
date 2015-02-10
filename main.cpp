5c0f5c5a6580ba37a5adbac56e87d30c0bbc3ec8
antlord <antlord92@gmail.com>
2015-02-11 00:55:02 +0800

    spiral from dots has been drawn

--- main.cpp
+++ main.cpp
@@ -2,13 +2,30 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <GL/glext.h>
+#include <math.h>
+#define GL_PI 3.1415f
 
 void RenderScene()
 {
+    GLfloat x,y,z,angle;
+    GLfloat xRot = 45.0, yRot = 45.0;
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.,0.,0.);
-    glRectf(-25.,25.,25.,-25.);
-    glFlush();
+    glPushMatrix();
+    glRotatef(xRot, 1., 0., 0.);
+    glRotatef(yRot, 0., 1., 0.);
+    glBegin(GL_POINTS);
+    {
+        z = -50.;
+        for (angle = 0.; angle <= (2. * GL_PI) * 3.; angle += 0.1f) {
+            x = 50.f * sin(angle);
+            y = 50.f * cos(angle);
+            glVertex3f(x,y,z);
+            z += 0.5;
+        }
+    }
+    glEnd();
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
 void ChangeSize(GLsizei width, GLsizei height)
@@ -27,12 +44,12 @@ void ChangeSize(GLsizei width, GLsizei height)
     if (width <= height) {
         glOrtho(-100. /* left */, 100. /* right */,
                 -100 / ASPECT_RATIO /* bottom */, 100 / ASPECT_RATIO /* top */,
-                -1. /* near */, 1./* far */);
+                -100. /* near */, 100./* far */);
     }
     else {
         glOrtho(-100 * ASPECT_RATIO /* left */, 100 * ASPECT_RATIO /* right */,
                 -100. /* back */, 100. /* front */,
-                -1. /* near */, 1./* far */);
+                -100. /* near */, 100./* far */);
     }
 
     glMatrixMode( GL_MODELVIEW );   // Выбор матрицы вида модели
@@ -42,10 +59,9 @@ void ChangeSize(GLsizei width, GLsizei height)
 void SetupRC()
 {
     glClearColor(0., 0., 0., 1.);
-
     glShadeModel( GL_SMOOTH );
-
     glClearDepth( 1.0f );              // Разрешить очистку буфера глубины
+    glColor3f(0., 1., 0.);
     glEnable( GL_DEPTH_TEST );            // Разрешить тест глубины
     glDepthFunc( GL_LEQUAL );            // Тип теста глубины
 }
@@ -53,7 +69,7 @@ void SetupRC()
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutCreateWindow("Sample");
     glutDisplayFunc(RenderScene);
     glutReshapeFunc(ChangeSize);
